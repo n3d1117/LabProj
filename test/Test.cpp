70031b5d28762e65da321ebbc6418fc1afeaf94b
ned <ned@appd.be>
2016-07-01 00:09:08 +0200

    Added test dir

---
+++ test/Test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ned on 30/06/16.
+//
+
+#include "gtest/gtest.h"
+#include "../LoadResources.h"
+
+TEST(LoadResources, test) {
+    ASSERT_EQ(0, 0);
+}
---
+++ test/runAllTests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ned on 30/06/16.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
