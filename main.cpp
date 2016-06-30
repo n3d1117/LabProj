1be1ec1ad618833f2da8f09b9d63d92a75120e6f
ned <ned@appd.be>
2016-06-30 22:42:34 +0200

    Added QT Integration, Progress Bar Test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <QApplication>
+#include "LoadResources.h"
 
 using namespace std;
 
-int main() {
-    cout << "This is a test!" << endl;
-    return 0;
+int main(int argc, char *argv[]) {
+
+    QApplication app(argc, argv);
+
+    LoadResources load;
+    load.setProgressBarValue(75);
+    load.showProgressBar();
+
+    return app.exec();
+
 }
